f110e6af9d929ebe6b578b3de110269da9ddf5ea
Michael Waide <mvwaide@gmail.com>
2020-07-03 19:41:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,34 +2,34 @@ require 'pry'
 def new_hash	def new_hash
 
   hash = {}
-end	
+end
 
 
 def actor	def actor
 
   dwayne = { name: 'Dwayne The Rock Johnson' }
-end	
+end
 
 
 def monopoly	def monopoly
 	monopoly = {}		monopoly = { railroads: {} }
 
 
-end	
+end
 
 
 def monopoly_with_second_tier
 	monopoly = { railroads: { pieces: 4, names: {}, rent_in_dollars: {} } }
-end	
+end
 
 
 def monopoly_with_third_tier
 				monopoly = { railroads: { pieces: 4, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {} }, rent_in_dollars: { one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 } } }
-end	
+end
 
 
 def monopoly_with_fourth_tier
 
 	monopoly = { railroads: { pieces: 4, names: {reading_railroad: {"mortgage_value" => '$100'}, pennsylvania_railroad: {"mortgage_value" => '$200'}, b_and_o_railroad: {"mortgage_value" => '$400'}, shortline: {"mortgage_value" => '$800'} }, rent_in_dollars: { one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 } } }
   #binding.pry
-end	
+end
